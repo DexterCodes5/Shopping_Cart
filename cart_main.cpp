e7c74bf77b9da23617a91f381699937102d76f3f
dexterlab_5 <103762164+DexterCodes5@users.noreply.github.com>
2022-10-23 16:38:31 +0300

    Update cart_main.cpp

--- cart_main.cpp
+++ cart_main.cpp
@@ -2,29 +2,22 @@
 #include <vector>
 #include "datamodel.h"
 
-std::vector<Product> allProd {
-    Product{1,"Apple",3},
-    Product{3,"Mango",6},
-    Product{2,"Guava",7},
-    Product{5,"Banana",4},
-    Product{4,"Strawberry",9},
-    Product{6,"Pineapple",11}
+std::vector<std::shared_ptr<Product>> allProd {
+    std::make_shared<Product>(1,"Apple",3),
+    std::make_shared<Product>(3,"Mango",6),
+    std::make_shared<Product>(2,"Guava",7),
+    std::make_shared<Product>(5,"Banana",4),
+    std::make_shared<Product>(4,"Strawberry",9),
+    std::make_shared<Product>(6,"Pineapple",11)
 };
 
-void printProd() {
-    std::cout << "Available Products \n";
-    for (Product p: allProd) {
-        std::cout << p.getDisplayName();
-    }
-}
-
-Product *chooseProduct() {
+std::shared_ptr<Product> chooseProduct() {
     // Display the list of products
     std::string prodList{};
     std::cout << "Available Products \n";
     
-    for (Product p: allProd) {
-       prodList.append(p.getDisplayName());
+    for (std::shared_ptr<Product> p: allProd) {
+       prodList.append(p->getDisplayName());
     }
     std::cout << prodList << "\n";
     std::cout << "--------------------------\n";
@@ -32,8 +25,8 @@ Product *chooseProduct() {
     std::cin >> choice;
     
     for (size_t i = 0; i < allProd.size(); i++) {
-        if (allProd.at(i).getShortName() == std::toupper(choice)) {
-            return &allProd.at(i);
+        if (allProd.at(i)->getShortName() == std::toupper(choice)) {
+            return allProd.at(i);
         }
     }
     
@@ -41,13 +34,13 @@ Product *chooseProduct() {
     return nullptr;
 }
 
-bool checkout(Cart &c) {
-    if (c.isEmpty()) {
+bool checkout(std::shared_ptr<Cart> c) {
+    if (c->isEmpty()) {
         std::cerr << "Error, cart is empty\n";
         return false;
     }
     
-    int total = c.getTotal();
+    int total = c->getTotal();
     std::cout << "Pay in cash ";
     
     int paid{};
@@ -66,7 +59,7 @@ bool checkout(Cart &c) {
 
 int main() {
     char act{};
-    Cart cart;
+    std::shared_ptr<Cart> cart = std::make_shared<Cart>();
     
     while (true) {
         std::cout << "Select an action - a.Add item v.View cart c.Checkout q.Quit\n";
@@ -76,27 +69,27 @@ int main() {
             // Add to cart
             // View products + Choose product + Add to the cart
             std::cout << "  Add to cart\n";
-            Product *p = chooseProduct();
+            std::shared_ptr<Product> p = chooseProduct();
             if (p != nullptr) {
                 std::cout << "Added to the Cart " << p->getDisplayName() << "\n";
-                cart.addProduct(*p);
+                cart->addProduct(p);
             }
         }
         else if (act == 'V') {
             // View cart
             std::cout << "  View cart\n";
             std::cout << "--------------------------\n";
-            std::cout << cart.viewCart();
+            std::cout << cart->viewCart();
             std::cout << "--------------------------\n";
         }
         else if (act == 'C') {
             // View cart
             std::cout << "  Checkout\n";
             std::cout << "--------------------------\n";
-            std::cout << cart.viewCart();
+            std::cout << cart->viewCart();
             std::cout << "--------------------------\n";
             // Checkout
-            if (checkout(cart)) cart.emptyCart();
+            if (checkout(cart)) cart->emptyCart();
         }
         else if (act == 'Q') {
             std::cout << "Good Bye.\n";
